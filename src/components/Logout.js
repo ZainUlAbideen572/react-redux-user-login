ecba53c853160b15759e211b48fa27eed0ce2640
Zain <mdzayan572@gmail.com>
2023-06-26 21:35:29 +0530

    first commit

---
+++ src/components/Logout.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Logout=()=>{
+    return <div>
+        <h1>
+            welcome <span className="user_name">Md Zayan</span>
+            <br/>
+            <button className="btn btn-primary">Logout</button>
+        </h1>
+    </div>
+}
+export default Logout;
